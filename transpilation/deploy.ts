68cc9e80bfdc17c3e2771dbda3f5c0ecd03f4d86
Misha Kaletsky <misha.kaletsky@gmail.com>
2016-10-26 12:39:25 +0100

    Commit the changes.

--- transpilation/deploy.ts
+++ transpilation/deploy.ts
@@ -9,19 +9,21 @@ function cmd(command: string): string {
     return result.stdout.trim().replace("\r\n", "\n");
 }
 
-const currentBranch = cmd("git rev-parse --abbrev-ref HEAD");
-if (currentBranch !== "master") {
-    throw new Error("This script should only be run on master. You're on " + currentBranch);
+const initialBranch = cmd("git rev-parse --abbrev-ref HEAD");
+if (initialBranch !== "master") {
+    throw new Error("This script should only be run on master. You're on " + initialBranch);
 }
 const changes = cmd("git status --porcelain");
 if (changes) {
     throw new Error("This script shouldn't be run when you've got working copy changes.");
 }
 
+const lastCommit = cmd("git rev-parse HEAD");
+
 cmd("git branch -f gh-pages");
 cmd("git checkout gh-pages");
 
-const toDeploy = [ "index.html", "dist/slime.js", ".gitignore", "transpilation/deploy.ts" ];
+const toDeploy = [ "index.html", "dist/slime.js", ".gitignore" ];
 
 fs.writeFileSync(".gitignore", "*", "utf8");
 
@@ -31,6 +33,8 @@ const toRemove = trackedFiles.filter(f => toDeploy.indexOf(f) === -1)
 toRemove.forEach(f => cmd(`git rm -rf ${f}`));
 
 toDeploy.forEach(f => cmd(`git add -f ${f}`));
+
+cmd(`git commit -m "Auto-deploy of commit ${lastCommit} on branch ${initialBranch}.`);
 
 console.log("Exiting.");
 process.exit();
