a9daca4a9de4384f4d412442c65fb38dc1881487
Misha Kaletsky <misha.kaletsky@gmail.com>
2016-10-26 11:43:10 +0100

    Check that there are no changes before running deploy.

--- transpilation/deploy.ts
+++ transpilation/deploy.ts
@@ -1,8 +1,18 @@
 import fs = require("fs");
 import { exec } from "shelljs";
 
-exec("git branch -f gh-pages");
-exec("git checkout gh-pages");
+function cmd(command: string) {
+    const result: any = exec(command);
+    return result.stdout;
+}
+
+const porcelain = cmd("git status --porcelain");
+if (porcelain.trim()) {
+    throw new Error("This script shouldn't be run when you've got working copy changes.");
+}
+
+cmd("git branch -f gh-pages");
+cmd("git checkout gh-pages");
 
 const toDeploy = [ "index.html", "dist/slime.js", ".gitignore", "transpile/deploy.ts", "dist/transpile/deploy.js" ];
 
@@ -10,6 +20,6 @@ const gitignore = ["*"].concat(toDeploy.map(f => "!" + f)).join("\r\n");
 
 fs.writeFileSync(".gitignore", gitignore, "utf8");
 
-exec("git rm -rf");
+cmd("git rm -rf * --dry-run");
 
-toDeploy.forEach(f => exec(`git add ${f}`));
+toDeploy.forEach(f => cmd(`git add cmd{f}`));
