d4e06a10f2f095f8b8932fdbc166fd59a0f15ad8
Misha Kaletsky <misha.kaletsky@gmail.com>
2016-10-26 12:07:26 +0100

    Ensure master is current branch before running deploy script.

--- transpilation/deploy.ts
+++ transpilation/deploy.ts
@@ -6,6 +6,10 @@ function cmd(command: string): string {
     return result.stdout.trim().replace("\r\n", "\n");
 }
 
+const currentBranch = cmd("git rev-parse --abbrev-ref HEAD");
+if (currentBranch !== "master") {
+    throw new Error("This script should only be run on master. You're on " + currentBranch);
+}
 const changes = cmd("git status --porcelain");
 if (changes) {
     throw new Error("This script shouldn't be run when you've got working copy changes.");
