9c46cf34ffe8465b6f8442ad9d0da72354388333
Misha Kaletsky <misha.kaletsky@gmail.com>
2016-10-28 16:25:03 +0100

    Try compiling a file from script.

---
+++ Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    public String getGreeting() {
+        return "hello";
+    }
+}
---
+++ test-java.ts
@@ -0,0 +1,12 @@
+import { exec, ExecOptions, mkdir } from "shelljs";
+
+function cmd(command: string, options?: ExecOptions): string {
+    const result: any = exec(command, options);
+    if (result.code !== 0) {
+        throw new Error(result.stderr);
+    }
+    return result.stdout.trim().replace("\r\n", "\n");
+}
+mkdir("dist");
+const result = cmd(`javac Hello.java -d dist`);
+console.log(result);
