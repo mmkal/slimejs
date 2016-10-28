ac67427197df32c907669722afc2fc0fec839e6d
Misha Kaletsky <misha.kaletsky@gmail.com>
2016-10-28 20:23:15 +0100

    Don't print success message after every successful command.

--- ts/util/cmd.ts
+++ ts/util/cmd.ts
@@ -2,13 +2,13 @@ import chalk = require("chalk");
 import { exec, ExecOptions } from "shelljs";
 
 export default function cmd(command: string, options?: ExecOptions): string {
-    if (!options || !options.silent) {
+    const printCommand = !options || !options.silent;
+    if (printCommand) {
         console.log(chalk.blue(command));
     }
     const result: any = exec(command, options);
     if (result.code !== 0) {
         throw new Error(result.stderr);
     }
-    console.log(chalk.green("Command success: " + command));
     return result.stdout.trim().replace("\r\n", "\n");
 }
