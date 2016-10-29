fd3fc9dbd52b7b4b989d0a3773665fcc07890489
Misha Kaletsky <misha.kaletsky@gmail.com>
2016-10-29 18:32:41 +0100

    Add a plugin to webpack config to make it easier to see when a watched compilation has finished.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
+function RandomDotsPlugin() {
+}
+RandomDotsPlugin.prototype.apply = function(compiler) {
+    compiler.plugin("compile", function() {
+        const dots = ["Compiling"];
+        const number = Math.floor(Math.random() * 100);
+        for (let i = 0; i < number; i++) {
+            dots.push(".");
+        }
+        console.log(dots.join(""));
+    });
+    compiler.plugin("after-compile", function(compilation, callback) {
+        const dots = ["Done"];
+        const number = Math.floor(Math.random() * 100);
+        for (let i = 0; i < number; i++) {
+            dots.push("!");
+        }
+        console.log(dots.join(""));
+        callback();
+    });
+};
+
 module.exports = {
     entry: "./ts/client/slime",
     output: {
@@ -11,5 +33,6 @@ module.exports = {
         loaders: [
             { test: /\.ts$/, loader: "ts-loader" }
         ]
-    }
+    },
+    plugins: [ new RandomDotsPlugin() ]
 };
